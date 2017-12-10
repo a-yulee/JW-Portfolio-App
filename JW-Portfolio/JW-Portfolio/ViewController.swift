149a3b042e17df3800eeec452d0271dba4294261
Jonny Wang <qbs1864@gmail.com>
2017-12-10 15:45:52 -0500

    Add files via upload

---
+++ JW-Portfolio/JW-Portfolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JW-Portfolio
+//
+//  Created by Jonny Wang on 12/10/17.
+//  Copyright © 2017 JW Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
